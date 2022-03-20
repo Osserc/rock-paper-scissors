c8b605e36a37ffa3959b030db4fd8b416a1d1238
Osserc <rstefanofil@hotmail.com>
2022-03-20 22:01:49 +0100

    Fix functions
    Fixed the embarrassing mistake with the comparisons
    Updated the function to check for string identity

--- javascript.js
+++ javascript.js
@@ -4,34 +4,39 @@ function computerPlay(min, max) {
 
 let computerSelection = computerPlay(1, 3);
 console.log(computerSelection);
-let playerSelection = computerPlay(1, 3);
+let playerInput = 'SCISSORS';
+let playerSelection;
+playerSelection = playerChoiceConversion(playerInput);
+
 console.log(playerSelection);
+let gamesWon = 0;
+let gamesLost = 0;
 
 //Rock = 1, Paper = 2, Scissors = 3
 
-function round(playerSelection, computerSelection) {
-    if (((playerSelection = 1) && (computerSelection = 3)) ||
-    ((playerSelection = 2) && (computerSelection = 1)) ||
-    ((playerSelection = 3) && (computerSelection = 2))) {
+function playerChoiceConversion(playerInput) {
+    if (playerInput.localeCompare('rock', 'en', {sensitivity: 'base'}) == 0) {
+        return playerSelection = 1;
+    } else if (playerInput.localeCompare('paper', 'en', {sensitivity: 'base'}) == 0) {
+        return playerSelection = 2;
+    } else if (playerInput.localeCompare('scissors', 'en', {sensitivity: 'base'}) == 0) {
+        return playerSelection = 3;
+    } else return;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (((playerSelection == 1) && (computerSelection == 3)) ||
+    ((playerSelection == 2) && (computerSelection == 1)) ||
+    ((playerSelection == 3) && (computerSelection == 2))) {
         gamesWon++;
         console.log('You won!');
-    } else if (((playerSelection = 1) && (computerSelection = 3)) ||
-        ((playerSelection = 2) && (computerSelection = 1))||
-        ((playerSelection = 3) && (computerSelection = 2))) {
+    } else if (((playerSelection == 1) && (computerSelection == 2)) ||
+        ((playerSelection == 2) && (computerSelection == 3))||
+        ((playerSelection == 3) && (computerSelection == 1))) {
         gamesLost++;
         console.log('You lost...');
     } else {
     console.log('It\'s a tie, hang in there...!');
     return;
     }
-}
-
-function playerChoiceConversion(playerInput) {
-    if (playerInput = '\rock\i') {
-        playerSelection = 1;
-    } else if (playerInput = '\paper\i') {
-        playerSelection = 2;
-    } else if (playerInput = '\scissors\i') {
-        playerSelection = 3;
-    } else return;
 }
