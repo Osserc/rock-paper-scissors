77a34e2422d189678d93356c5fb309aadb0a10bf
Osserc <rstefanofil@hotmail.com>
2022-03-23 15:22:49 +0100

    Refactor code to introduce a UI
    Completely restructured the code to have the game be played
    through buttons
    Added record keeping

--- javascript.js
+++ javascript.js
@@ -1,51 +1,51 @@
 //Setting up variables to count the score
 let gamesWon = 0;
 let gamesLost = 0;
+let gamesPlayed = 0;
 
-for (let gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
+const btn = document.querySelectorAll('.btn').forEach(btn => btn.addEventListener('click', function handler (e) {
+    console.log(e);
+    if (e.target.id == 'rock') {
+        playerSelection = 1;
+        console.log(playerSelection);
+    } else if (e.target.id == 'paper') {
+            playerSelection = 2;
+            console.log(playerSelection);
+        } else {
+            playerSelection = 3;
+            console.log(playerSelection);
+        }
 
-    //Computer chooses
     let computerSelection = computerPlay(1, 3);
 
-    //Player chooses
-    let playerInput = window.prompt('Rock, paper or scissors?');
-    //Choice gets converted to a number: Rock = 1, Paper = 2, Scissors = 3
-    let playerSelection;
-    playerSelection = playerChoiceConversion(playerInput);
-
-    //Round starts
     playRound(playerSelection, computerSelection);
 
-    //Terminating the loop early with 3 victories or losses
-    if (gamesWon == 3) {
-        console.log('You won ' + gamesWon + ' to ' + gamesLost + ', congratulations!')
-        break;
-    } else if (gamesLost == 3) {
-        console.log('You lost ' + gamesLost + ' to ' + gamesWon + ', better luck next time...');
-        break;
-    }
-    
-    //Ending with all 5 games played but no decisive win
-    if ((gamesPlayed == 4) && (gamesWon > gamesLost)) {
-        console.log('You won ' + gamesWon + ' to ' + gamesLost + ', congratulations!');
-    } else if ((gamesPlayed == 4) && (gamesWon < gamesLost)) {
-        console.log('You lost ' + gamesLost + ' to ' + gamesWon + ', better luck next time...');
-    }
+    gamesPlayed++;
 
-}
+    if (gamesWon == 5) {
+            const container = document.querySelector('.results');
+
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = 'Congratulations! You won the game ' + gamesWon + ' to ' + gamesLost + '!';
+
+            container.appendChild(content);
+            const element = document.querySelectorAll('.btn').forEach(element => element.removeEventListener('click', handler))
+        } else if (gamesLost == 5) {
+            const container = document.querySelector('.results');
+
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = 'Game over! You lost ' + gamesWon + ' to ' + gamesLost + '.';
+
+            container.appendChild(content);
+            const element = document.querySelectorAll('.btn').forEach(element => element.removeEventListener('click', handler))
+        }
+
+}))
 
 function computerPlay(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function playerChoiceConversion(playerInput) {
-    if (playerInput.localeCompare('rock', 'en', {sensitivity: 'base'}) == 0) {
-        return playerSelection = 1;
-    } else if (playerInput.localeCompare('paper', 'en', {sensitivity: 'base'}) == 0) {
-        return playerSelection = 2;
-    } else if (playerInput.localeCompare('scissors', 'en', {sensitivity: 'base'}) == 0) {
-        return playerSelection = 3;
-    } else return;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -53,16 +53,44 @@ function playRound(playerSelection, computerSelection) {
     ((playerSelection == 2) && (computerSelection == 1)) ||
     ((playerSelection == 3) && (computerSelection == 2))) {
         gamesWon++;
-        console.log('You won!');
+        const container = document.querySelector('.results');
+
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = 'You won! The score is ' + gamesWon + ' to ' + gamesLost + '.';
+
+        container.appendChild(content);
         return gamesWon;
     } else if (((playerSelection == 1) && (computerSelection == 2)) ||
         ((playerSelection == 2) && (computerSelection == 3))||
         ((playerSelection == 3) && (computerSelection == 1))) {
         gamesLost++;
-        console.log('You lost...');
+        const container = document.querySelector('.results');
+
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = 'You lost... the score is ' + gamesWon + ' to ' + gamesLost + '.';
+
+        container.appendChild(content);
         return gamesLost;
     } else {
-    console.log('It\'s a tie, hang in there...!');
-    return;
+        const container = document.querySelector('.results');
+
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = 'It\'s a tie, hang in there! The score is ' + gamesWon + ' to ' + gamesLost + '.';
+
+        container.appendChild(content);
+        return;
     }
+}
+
+function counter (gamesWon) {
+    const container = document.querySelector('.results');
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = 'You lost... the score is ' + gamesWon + ' to ' + gamesLost + '.';
+
+    container.appendChild(content);
 }
