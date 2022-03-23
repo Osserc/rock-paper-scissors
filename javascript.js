0a50a33cc6ac6cf0dfc107ba448ef411e2a1639a
Osserc <rstefanofil@hotmail.com>
2022-03-23 18:58:51 +0100

    Finish css

--- javascript.js
+++ javascript.js
@@ -1,43 +1,39 @@
 //Setting up variables to count the score
 let gamesWon = 0;
 let gamesLost = 0;
-let gamesPlayed = 0;
 
 const btn = document.querySelectorAll('.btn').forEach(btn => btn.addEventListener('click', function handler (e) {
     console.log(e);
     if (e.target.id == 'rock') {
         playerSelection = 1;
-        console.log(playerSelection);
     } else if (e.target.id == 'paper') {
             playerSelection = 2;
-            console.log(playerSelection);
         } else {
             playerSelection = 3;
-            console.log(playerSelection);
         }
 
     let computerSelection = computerPlay(1, 3);
 
     playRound(playerSelection, computerSelection);
 
-    gamesPlayed++;
-
     if (gamesWon == 5) {
-            const container = document.querySelector('.results');
+            const container = document.querySelector('.ongoing');
 
             const content = document.createElement('div');
             content.classList.add('content');
-            content.textContent = 'Congratulations! You won the game ' + gamesWon + ' to ' + gamesLost + '!';
+            content.textContent = 'Congratulations, magister! You successfully defended your honor!';
 
+            container.removeChild(container.lastChild);
             container.appendChild(content);
             const element = document.querySelectorAll('.btn').forEach(element => element.removeEventListener('click', handler))
         } else if (gamesLost == 5) {
-            const container = document.querySelector('.results');
+            const container = document.querySelector('.ongoing');
 
             const content = document.createElement('div');
             content.classList.add('content');
-            content.textContent = 'Game over! You lost ' + gamesWon + ' to ' + gamesLost + '.';
+            content.textContent = 'By the gods! Your champions failed you! Exile is your only option now, magister...';
 
+            container.removeChild(container.lastChild);
             container.appendChild(content);
             const element = document.querySelectorAll('.btn').forEach(element => element.removeEventListener('click', handler))
         }
@@ -53,40 +49,66 @@ function playRound(playerSelection, computerSelection) {
     ((playerSelection == 2) && (computerSelection == 1)) ||
     ((playerSelection == 3) && (computerSelection == 2))) {
         gamesWon++;
-        const container = document.querySelector('.results');
+        const container = document.querySelector('.ongoing');
 
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'You won! The score is ' + gamesWon + ' to ' + gamesLost + '.';
+        content.textContent = 'Your champion triumphed!';
 
+        container.removeChild(container.lastChild);
         container.appendChild(content);
+
+        const score = document.querySelector('.score-yours .score-number');
+
+        const point = document.createElement('div');
+        point.classList.add('point');
+        point.textContent = gamesWon;
+
+        score.removeChild(score.lastChild);
+        score.appendChild(point);
+
         return gamesWon;
     } else if (((playerSelection == 1) && (computerSelection == 2)) ||
         ((playerSelection == 2) && (computerSelection == 3))||
         ((playerSelection == 3) && (computerSelection == 1))) {
         gamesLost++;
-        const container = document.querySelector('.results');
+        const container = document.querySelector('.ongoing');
 
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'You lost... the score is ' + gamesWon + ' to ' + gamesLost + '.';
+        content.textContent = 'Your champion lost...';
 
+        container.removeChild(container.lastChild);
         container.appendChild(content);
+
+        const score = document.querySelector('.score-veknos .score-number');
+
+        const point = document.createElement('div');
+        point.classList.add('point');
+        point.textContent = gamesLost;
+
+        score.removeChild(score.lastChild);
+        score.appendChild(point);
+
         return gamesLost;
+
     } else {
-        const container = document.querySelector('.results');
+        const container = document.querySelector('.ongoing');
 
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'It\'s a tie, hang in there! The score is ' + gamesWon + ' to ' + gamesLost + '.';
+        content.textContent = 'It\'s a tie, hang in there!';
 
+        container.removeChild(container.lastChild);
         container.appendChild(content);
+
         return;
     }
+
 }
 
 function counter (gamesWon) {
-    const container = document.querySelector('.results');
+    const container = document.querySelector('.ongoing');
 
     const content = document.createElement('div');
     content.classList.add('content');
